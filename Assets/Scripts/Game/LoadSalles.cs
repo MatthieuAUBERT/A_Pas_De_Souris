5f8320a43385c73aa07abee15e879cd6c15ace54
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2021-12-06 17:11:05 +0100

    TomJet'aimebien

--- Assets/Scripts/Game/LoadSalles.cs
+++ Assets/Scripts/Game/LoadSalles.cs
@@ -4,20 +4,8 @@ using UnityEngine.SceneManagement;
 public class LoadSalles : MonoBehaviour
 {
 
-    public void Salle1(string salle)
+    public void Salle(string salle)
     {
         SceneManager.LoadScene(salle);
-    }
-    public void Salle2()
-    {
-        SceneManager.LoadScene("Salle 2");
-    }
-    public void Salle3()
-    {
-        SceneManager.LoadScene("Salle 3");
-    }
-    public void Salle4()
-    {
-        SceneManager.LoadScene("Salle 4");
     }
 }
