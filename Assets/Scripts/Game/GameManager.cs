0907adeef66398651848fb1b7f95e3444d20d3a0
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2021-12-10 15:20:51 +0100

    VersionFonctionnelleALPHAV1
    Deux/Trois Retouches nécessaires
    Reste l'implémentation des assets

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        clues = new List<string>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Game/IndicesManager.cs
+++ Assets/Scripts/Game/IndicesManager.cs
@@ -5,10 +5,20 @@ public class IndicesManager : MonoBehaviour
 {
     public GameObject IndicesUI;
     public GameObject BananeUI;
+    public GameObject HDFUI;
+    public GameObject CravateUI;
+    public GameObject MaquillageUI;
+
+    public GameObject AccusationUI;
+
+    public string toCheck;
+    public string toCheck2;
+    public string toCheck3;
+    public string toCheck4;
 
     GameManager gm;
 
-    private void Awake()
+    public void Start()
     {
         gm = ManagerManager.Instance.gameManager;
     }
@@ -16,14 +26,29 @@ public class IndicesManager : MonoBehaviour
     public void SetIndicesUIActive(bool a)
     {
         IndicesUI.SetActive(a);
-        if (gm.clues.IndexOf("Banane") >= 0)
+        Debug.Log(gm);
+        Debug.Log(gm.clues);
+
+        if (gm.clues.IndexOf(toCheck) >= 0)
         {
             BananeUI.SetActive(true);
         }
-        else
+
+        if (gm.clues.IndexOf(toCheck2) >= 0)
         {
-            Debug.Log("No");
+            HDFUI.SetActive(true);
         }
+
+        if (gm.clues.IndexOf(toCheck3) >= 0)
+        {
+            CravateUI.SetActive(true);
+        }
+
+        if (gm.clues.IndexOf(toCheck4) >= 0)
+        {
+            MaquillageUI.SetActive(true);
+        }
+
     }
 
     public void SetUnactiveIndicesUI(bool a)
@@ -33,6 +58,12 @@ public class IndicesManager : MonoBehaviour
 
     public void Accusation()
     {
+        IndicesUI.SetActive(false);
+        AccusationUI.SetActive(true);
+    }
 
+    public void SetUnactiveAccusationUI(bool a)
+    {
+        AccusationUI.SetActive(a);
     }
 }
